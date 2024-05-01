23c809c56a982bbabd1851543d47636264111c1e
ElMansari-Youssef <91879610+ElMansari-Youssef@users.noreply.github.com>
2024-05-02 00:34:57 +0200

    initial commit
    test

---
+++ Code.js
@@ -0,0 +1,52 @@
+async function GetData() {
+
+    try {
+
+
+        let tbx = document.getElementById("tbx")
+        let image = document.getElementById("image1");
+        let PokeName = document.querySelectorAll(".card-content span")[0];
+        let idText = document.querySelectorAll(".card-content span")[1];
+
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${tbx.value}`)
+        let data = await response.json();
+
+
+        image.src = data.sprites.front_default;
+        PokeName.innerHTML = data.name
+        idText.textContent = "#" + data.id
+
+        console.log(data.moves[0].move.name)
+        console.log(data.moves[2].move.name)
+        console.log(data.weight + " kg")
+        console.log(data.types[0].type.name)
+       
+        AddCard();
+
+    } catch (error) {
+
+        if (tbx == "")
+            alert("please fill the txtbox");
+        else alert("something went wrong" + error);
+    }
+
+}
+
+
+ function AddCard() {
+
+
+    let cardContainer = document.getElementsByClassName("card-container")
+
+    let card = document.getElementsByClassName("card");
+
+    let cardClone = cardContainer[0].appendChild(card[0].cloneNode(true))
+
+
+    card[0].style.display = "none";
+
+    cardClone.style = "display:block;"
+
+    return cardClone
+
+}
